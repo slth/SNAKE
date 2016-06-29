0318bc20a758a56f0712e5d17836ef8e55ae9ad4
xalexey <saleksey67@gmail.com>
2016-06-29 23:58:19 +0300

    create project snake

---
+++ snake/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: salek
+ * Date: 29.06.2016
+ * Time: 23:54
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace snake
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
