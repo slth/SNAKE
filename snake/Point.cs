cbb64fb26717c9938c6d514eb03ed1e03c3d3d1c
xalexey <saleksey67@gmail.com>
2016-06-30 00:47:02 +0300

    Lesson 3, added Point Class

---
+++ snake/Point.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: salek
+ * Date: 30.06.2016
+ * Time: 0:33
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace snake
+{
+	class Point
+	{
+		public int x;
+		public int y;
+		public char sym;
+
+		public void Draw() {
+			Console.SetCursorPosition(x, y);
+			Console.Write(sym);	
+		}
+	}
+}
