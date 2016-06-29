29b0e5681f2e8dcbf0a540085af762f28bd82bd4
xalexey <saleksey67@gmail.com>
2016-06-30 00:59:28 +0300

    Added Construction & execute incapsulation Strings

--- snake/Point.cs
+++ snake/Point.cs
@@ -15,6 +15,13 @@ namespace snake
 		public int x;
 		public int y;
 		public char sym;
+		
+		public Point(int _x, int _y, char _sym) {
+			x = _x;
+			y = _y;
+			sym = _sym;
+			
+		}
 
 		public void Draw() {
 			Console.SetCursorPosition(x, y);
