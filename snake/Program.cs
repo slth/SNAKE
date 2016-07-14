e9bddad16e214fe453f84b7946721e8b0e27b9a9
xalexey <saleksey67@gmail.com>
2016-07-14 03:25:22 +0300

    thats work! last not! (reverted from commit 3900431d94af748ac03d958301cf318ea71615b4)

--- snake/Program.cs
+++ snake/Program.cs
@@ -7,6 +7,10 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace snake
 {
@@ -19,6 +23,26 @@ namespace snake
 			
 			Point p2 = new Point(5, 3, '#');
 			p2.Draw();
+			
+			List<int> numList = new List<int>();
+			numList.Add(0);
+			numList.Add(1);
+			numList.Add(2);
+			
+			int x = numList[0];			
+			int y = numList[1];			
+			int z = numList[2];
+			
+			foreach(int i in numList)
+			{
+				Console.WriteLine(i);
+			}	
+			
+			numList.RemoveAt(0);
+			
+			List<Point> pList = new List<Point>();
+			pList.Add(p1);
+			pList.Add(p2);
 			
 			Console.ReadLine();
 		}
